788c99816afcc487cae13c83d31ab0022dc57793
HardikDhuri <hardikdhuri8347@gmail.com>
2022-08-14 13:45:19 +0530

    created a registration branch viewbinding in RegistrationActivity.kt google auth in RegistrationActivity.kt fails on tap

---
+++ app/src/main/java/com/example/samvach/LoginActivity.kt
@@ -0,0 +1,12 @@
+// Kripiya Code likhteh waqt savdhani barteh ek bug project ko bht.... bht.... crash karwasakti h
+package com.example.samvach
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/example/samvach/MainActivity.kt
+++ app/src/main/java/com/example/samvach/MainActivity.kt
@@ -1,16 +1,29 @@
+// Kripiya Code likhteh waqt savdhani barteh ek bug project ko bht.... bht.... crash karwasakti h
 package com.example.samvach
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<TextView>(R.id.textview1)
+        logout_btn.setOnClickListener {
+            auth.signOut()
+            redirectToLogin()
+        }
 
+    }
 
+    private fun redirectToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
