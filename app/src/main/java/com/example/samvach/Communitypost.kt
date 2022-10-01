64e22fe193097c3bef0d7fe52e494a05faf08aca
Prasad kapse <prasadkapse143@gmail.com>
2022-10-01 11:00:43 +0530

    created community post

---
+++ app/src/main/java/com/example/samvach/Communitypost.kt
@@ -0,0 +1,15 @@
+package com.example.samvach
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Communitypost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_communitypost)
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/samvach/password/Password.kt
@@ -0,0 +1,12 @@
+package com.example.samvach.password
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.samvach.R
+
+class Password : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password)
+    }
+}
